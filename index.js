27c7c8ae8a23846c3f521009af4955f8e8096a14
ASkeTizy <45819392+ASkeTizy@users.noreply.github.com>
2022-12-05 17:13:26 +0300

    refactor: add classes but it's not wotking yet

--- index.js
+++ index.js
@@ -1,66 +1,94 @@
 class Sketcher { 
+   #size = 16;
 
-}
-
-let size;
-function generateGrid(size) {
-	console.log(size);
-	const grid = document.createElement('div');
-	grid.classList.add("sketch");
-	let gridElem;
-	setCssGridSize(size);
-	for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-			gridElem = document.createElement('div');
-			gridElem.classList.add("sketch_elem");
-			grid.append(gridElem);
-		}
+	constructor(size) {
+		size = this.size;
 	}
-	return grid;
-}
-function setEventListeners(color) {
-	if (color == null) color = 'black';
-	let cells = document.getElementsByClassName("sketch_elem");
-	console.log(color);
-	[... cells].forEach((gridElem) => gridElem.addEventListener("mouseover", 
-						(e) => e.buttons == 1 ? setGridElemColor(gridElem,color) :null , false));
-}
-function setGridElemColor(gridElem, color) {
-	gridElem.style.backgroundColor = color;
-}
 
+	generateGrid() {
+		console.log(size);
+		const grid = document.createElement('div');
+		grid.classList.add("sketch");
+		let gridElem;
+		this.setCssGridSize(this.size);
+		for (let i = 0; i < this.size; i++) {
+        	for (let j = 0; j < this.size; j++) {
+				gridElem = document.createElement('div');
+				gridElem.classList.add("sketch_elem");
+				grid.append(gridElem);
+			}
+		}
+		return grid;
+	}
 
-function setCssGridSize(size) {
-	const r = document.querySelector(':root');
-	r.style.setProperty('--size', size)
-}
-
-const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-	output.innerHTML = this.value;
+	static setEventListeners(color) {
+		if (color == null) color = 'black';
+		let cells = document.getElementsByClassName("sketch_elem");
+		console.log(color);
+		[... cells].forEach((gridElem) => gridElem.addEventListener("mouseover", 
+							(e) => e.buttons == 1 ? this.setGridElemColor(gridElem,color) :null , false));
+	}
+	setGridElemColor(gridElem, color) {
+		gridElem.style.backgroundColor = color;
+	}
+	setCssGridSize(size) {
+		const r = document.querySelector(':root');
+		r.style.setProperty('--size', size);
+	}
+	rangeValueChanger(grid) {
+		const slider = document.getElementById("myRange");
+		const output = document.getElementById("demo");
+		output.innerHTML = slider.value;
+		slider.oninput = function() {
+			output.innerHTML = this.value;
+		}
+		slider.addEventListener('change', (e) => {
+			const core = document.getElementsByClassName("content")[0];
+			size = slider.value;
+			grid.remove();	
+			grid  = generateGrid(size);
+			core.appendChild(t);
+			setEventListeners();
+		})
+	}
 	
-
+}
+class ColorEvent {
+	#color = 'black';
+	constructor(color) {
+		color = this.color;
+	}
+	setColor(color) {
+		color = this.color;
+	}
+	getColor() {
+		return this.color;
+	}
+	colorListener() {
+		let input = document.getElementById('color_selector');
+		input.addEventListener('change', (e) => {
+			Sketcher.setEventListeners(e.target.value);
+		})
+	}
 }
 
-size = slider.value;
-let t  = generateGrid(size);
 
-const core = document.getElementsByClassName("content")[0];
-core.appendChild(t);
-setEventListeners();
-let input = document.getElementById('color_selector');
-console.log(input);
-input.addEventListener('change', (e) => {
-	setEventListeners(e.target.value)
-})
-slider.addEventListener('change', (e) => {
+
+function main() {
+	const sketcher = new Sketcher();
+	const colorEvent = new ColorEvent();
+	colorEvent.colorListener();
+	sketcher.generateGrid();
 	size = slider.value;
-	t.remove();	
-	t  = generateGrid(size);
+	let t  = generateGrid(size);
+	const core = document.getElementsByClassName("content")[0];
 	core.appendChild(t);
 	setEventListeners();
-})
+	console.log(input);
+}
+
+
+
+
+
 // document.body.append(grid);
