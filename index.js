532e0dbd05a7c953118f5c7f7f1e0ef0865d37d1
ASkeTizy <45819392+ASkeTizy@users.noreply.github.com>
2022-12-18 22:26:28 +0300

    add: color modes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 class Sketcher { 
    #size = 16;
-   #color = 'black'
+   #color = 'black';
+   #modeColorFlag = false;
 	generateGrid() {
 		const grid = document.createElement('div');
 		grid.classList.add("sketch");
@@ -11,7 +12,6 @@ class Sketcher {
         	for (let j = 0; j < this.#size; j++) {
 				gridElem = document.createElement('div');
 				gridElem.classList.add("sketch_elem");
-
 				grid.append(gridElem);
 			}
 		}
@@ -29,7 +29,11 @@ class Sketcher {
 							(e) => e.buttons == 1 ? this.setGridElemColor(gridElem) :null , false));
 	}
 	setGridElemColor(gridElem) {
-		gridElem.style.backgroundColor = this.#color;
+		if(this.#modeColorFlag === false) {
+			gridElem.style.backgroundColor = this.#color;
+		}else {
+			gridElem.style.backgroundColor = this.randomRGB();
+		}
 	}
 	setCssGridSize(size) {
 		const r = document.querySelector(':root');
@@ -39,7 +43,7 @@ class Sketcher {
 	set setSize(size) {
 		this.#size = size;
 	}
-	rangeValueChanger(grid) {
+	rangeValueChanger() {
 		const slider = document.getElementById("myRange");
 		const output = document.getElementById("demo");
 		output.innerHTML = slider.value;
@@ -51,51 +55,69 @@ class Sketcher {
 			const core = document.getElementsByClassName("content")[0];
 			document.getElementsByClassName("sketch")[0].remove();
 			this.#size = slider.value;
-			grid  = this.generateGrid(this.#size);
+			let grid  = this.generateGrid(this.#size);
 			core.appendChild(grid);
 			
 		})
 	}
 	colorListener() {
 		let input = document.getElementById('color_selector');
+		this.#color = input.value;
 		input.addEventListener('change', (e) => {
 			this.#color = e.target.value;
 		})
 	}
 	 erazer() {
 		const erazer = document.getElementsByClassName('erazer')[0];
-		erazer.addEventListener('click', () => {this.#color = 'white' })
+		erazer.addEventListener('click', () => {this.#color = 'white' })	
+	}
+	randomRGB() {
 		
-	 	
+			var x = Math.floor(Math.random() * 256);
+	  		var y = Math.floor(Math.random() * 256);
+	   	var z = Math.floor(Math.random() * 256);
+	  		var RGBColor = "rgb(" + x + "," + y + "," + z + ")";  
+	  		
+	  		return RGBColor;	
+	
+	}
+	colorMode() {
+		const color_mode = document.getElementsByClassName('color_mode')[0];
+		color_mode.addEventListener('click', () => this.#modeColorFlag = false );
+	}
+	randomMode() {
+		const random = document.getElementsByClassName('random')[0];
+		random.addEventListener('click', () => this.#modeColorFlag = true);
+	}
+	 cleaner () {
+	 	const cleaner = document.getElementsByClassName('cleaner')[0];
+	 	cleaner.addEventListener('click', () => {
+	 		const core = document.getElementsByClassName("content")[0];
+			const slider = document.getElementById("myRange");
+			document.getElementsByClassName("sketch")[0].remove();
+			this.#size = slider.value;
+			let grid = this.generateGrid();
+			core.appendChild(grid);
+	 	})
+		
 	}
 	
 }
 
 
-function cleaner () {
-	const core = document.getElementsByClassName("content")[0];
-	const slider = document.getElementById("myRange");
-	document.getElementsByClassName("sketch")[0].remove();
-	const sketcher = new Sketcher();
-	sketcher.setSize = slider.value;
-	grid  = sketcher.generateGrid(sketcher.getSize);
-			core.appendChild(grid);
-}
-function main() {
 
+function main() {
 	const sketcher = new Sketcher();
+	sketcher.rangeValueChanger();
 	sketcher.colorListener();
 	sketcher.setCssGridSize();
+	sketcher.randomMode();
+	sketcher.colorMode();
 	sketcher.erazer();
+	sketcher.cleaner();
 	const grid = sketcher.generateGrid();
-	sketcher.rangeValueChanger(grid);
-
-	
 }
 
 main();
 
 
-
-
-// document.body.append(grid);
